e6d9c220444f5a9d23f635e55dccbb2000c18e87
brandonshearin <bshearin15@gmail.com>
2017-08-07 20:23:46 -0400

    fixed heroku connection

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ var http = require('http');
 
 
 const publicPath = path.join(__dirname, '../public');
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
