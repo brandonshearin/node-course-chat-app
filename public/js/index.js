e25d8bed1ef5baaf19d83a9fad343d41d1886871
brandonshearin <bshearin15@gmail.com>
2017-08-08 10:45:23 -0400

    scroll to bottom autoscrolling

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,23 @@
 
 	var socket = io();
 
+
+function scrollToBottom() {
+	//selectors
+	var messages = jQuery('#messages');
+	var newMessage = messages.children('li:last-child');
+	//heights
+	var clientHeight = messages.prop('clientHeight');
+	var scrollTop = messages.prop('scrollTop');
+	var scrollHeight = messages.prop('scrollHeight');
+	var newMessageHeight = newMessage.innerHeight();
+	var lastMessageHeight = newMessage.prev().innerHeight();
+
+	if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+		messages.scrollTop(scrollHeight);
+	}
+};
+
 	socket.on('connect', () => {
 		console.log('connected to server');
 	});
@@ -19,6 +36,7 @@ socket.on('newMessage', function(message){
 	});
 
 	jQuery('#messages').append(html);
+	scrollToBottom();
 	// var formattedTime = moment(message.createdAt).format('h:mm a');
 	// console.log('New message', message);
 	// var li = jQuery('<li></li>');
@@ -36,6 +54,7 @@ socket.on('newLocationMessage', function(message){
 		url: message.url
 	});
 	jQuery('#messages').append(html);
+	scrollToBottom();
 	// var li = jQuery('<li></li>');
 	// var a = jQuery('<a target="_blank">My current location</a>');
 
